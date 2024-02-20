0de02579be635154ef4ec8ee85d5eb60d1dd972a
albertoboccolini <albertoboccolini03@gmail.com>
2024-02-20 14:43:37 +0100

    Fix TacoLogo width

--- app/components/public/TacoLogo.tsx
+++ app/components/public/TacoLogo.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const TacoLogo = () => {
     return (
-        <div>
-            <a href="/">
+        <div className="w-40 m-auto">
+            <a href="/" className="w-20">
                 <Image src={tacoLogo} alt="Logo Taco" className="font-bold text-2xl text-center m-auto"
                        width={100}
                        height={100}/>
