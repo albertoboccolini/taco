51d9dab1f6bd74c1321ffcc37aa4426738b2c8b2
Alberto Boccolini <albertoboccolini03@gmail.com>
2024-03-13 22:30:59 +0100

    Make UI responsive

--- app/components/homepage/SearchBar.tsx
+++ app/components/homepage/SearchBar.tsx
@@ -2,20 +2,21 @@ import React from "react";
 
 const SearchBar: React.FC<{ onSearchChange: ((value: string) => void) | null }> = ({onSearchChange}: any | null) => {
     return (
-        <div className="flex items-center rounded-lg mr-4 bg-white px-4 shrink-0">
+        <div className="flex items-center rounded-lg mr-4 bg-white px-4 shrink-0 w-full sm:w-auto">
             <div className="relative flex items-center w-full">
-                <SearchIcon className="absolute left-4 z-10 fill-white text-gray-400"/>
+                <SearchIcon className="absolute w-6 h-6 sm:w-auto sm:h-auto left-4 z-10 fill-white text-gray-400"/>
                 <input
-                    className="w-48 pl-12 pr-6 py-2 rounded-xl shrink-0 bg-white font-semibold placeholder:font-normal border-2 shadow"
+                    className="w-full pl-12 pr-6 py-2 rounded-xl shrink-0 bg-white font-semibold placeholder:font-normal border-2 shadow sm:w-auto"
                     placeholder="Search..."
                     maxLength={30}
                     type="search"
-                    onChange={(e) => onSearchChange(e.target.value)}
+                    onChange={(e) => onSearchChange && onSearchChange(e.target.value)}
                 />
             </div>
         </div>
     )
 }
+
 
 function SearchIcon(props: any) {
     return (
