21726f1037315a0396a3b1a604abf3a556615b36
Alberto Boccolini <albertoboccolini03@gmail.com>
2024-03-17 20:21:34 +0100

    Add drop layout

--- app/components/homepage/Layout.tsx
+++ app/components/homepage/Layout.tsx
@@ -4,9 +4,10 @@ import React from 'react';
 import tacoConverterLogo from '@/public/tacoConverterLogo.png';
 import tacoCalcLogo from '@/public/tacoCalcLogo.png';
 import tacoEncoderLogo from '@/public/tacoEncoderLogo.png';
-import tacoPasswords from '@/public/tacoPasswordsLogo.png';
+import tacoPasswordsLogo from '@/public/tacoPasswordsLogo.png';
 import tacoQRLogo from '@/public/tacoQRLogo.png';
 import tacoPaletteLogo from '@/public/tacoPaletteLogo.png';
+import tacoDropLogo from '@/public/tacoDropLogo.png';
 import Header from "@/app/components/public/Header";
 import Tool from "@/app/components/homepage/Tool";
 import SearchEngine from "@/app/components/homepage/SearchEngine";
@@ -14,48 +15,13 @@ import SearchEngine from "@/app/components/homepage/SearchEngine";
 const Layout: React.FC = () => {
 
     const tools = [
-        {
-            logo: tacoConverterLogo,
-            toolLink: "/tools/converter",
-            toolName: "taco converter",
-            category: "converter",
-            tags: ["taco", "file", "image", "format", "document converter", "pdf converter"]
-        },
-        {
-            logo: tacoQRLogo,
-            toolLink: "/tools/qrcode",
-            toolName: "taco QR",
-            category: "generator",
-            tags: ["taco", "QR code", "security", "scan", "mobile marketing", "contactless information sharing"]
-        },
-        {
-            logo: tacoEncoderLogo,
-            toolLink: "/tools/encoder",
-            toolName: "taco encoder",
-            category: "converter",
-            tags: ["taco", "data encoding", "security", "code obfuscation", "encryption", "text obfuscation"]
-        },
-        {
-            logo: tacoPasswords,
-            toolLink: "/tools/passwords",
-            toolName: "taco passwords",
-            category: "other",
-            tags: ["taco", "password generator", "security", "strong passwords", "account security", "password manager"]
-        },
-        {
-            logo: tacoPaletteLogo,
-            toolLink: "/tools/palette",
-            toolName: "taco palette",
-            category: "generator",
-            tags: ["taco", "color scheme", "design tool", "palette generator", "web design", "interior design inspiration"]
-        },
-        {
-            logo: tacoCalcLogo,
-            toolLink: "/tools/calc",
-            toolName: "taco calc",
-            category: "other",
-            tags: ["calculator", "math tool", "computation", "financial calculations", "educational tool"]
-        },
+        {logo: tacoConverterLogo, toolLink: "/tools/converter", toolName: "taco converter", category: "converter"},
+        {logo: tacoQRLogo, toolLink: "/tools/qrcode", toolName: "taco QR", category: "generator"},
+        {logo: tacoEncoderLogo, toolLink: "/tools/encoder", toolName: "taco encoder", category: "converter"},
+        {logo: tacoPasswordsLogo, toolLink: "/tools/passwords", toolName: "taco passwords", category: "other"},
+        {logo: tacoPaletteLogo, toolLink: "/tools/palette", toolName: "taco palette", category: "generator"},
+        {logo: tacoDropLogo, toolLink: "/tools/drop", toolName: "taco drop", category: "generator"},
+        {logo: tacoCalcLogo, toolLink: "/tools/calc", toolName: "taco calc", category: "other"}
     ];
 
 
