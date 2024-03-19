eaef43bb650c1371ac830575f73e8f3d8adda292
Alberto Boccolini <albertoboccolini03@gmail.com>
2024-03-19 11:34:58 +0100

    Add api workflow
    Add server.js
    
    Fix useSearchParams error
    
    Fix build loop
    
    add vercel.json
    
    Update server.js
    
    Update vercel.json
    
    Update vercel.json
    
    Bug fix
    
    Add style to room page
    
    Bug fix
    
    Rebase
    
    Bug fix
    
    Bug fix
    
    Change drop workflow
    
    CORS changes
    
    CORS changes
    
    CORS fix
    
    CORS fix
    
    CORS fix
    
    CORS fix
    
    CORS fix
    
    CORS fix
    
    CORS fix
    
    CORS fix
    
    Fix download button
    
    Fix download button
    
    Fix error messages
    
    Fix deploy

--- app/components/tools/drop/Engine.ts
+++ app/components/tools/drop/Engine.ts
@@ -1,6 +1,4 @@
 import React, {useState} from 'react';
-import Peer from 'simple-peer';
-import {v4} from "uuid";
 import {NotificationManager} from "@/app/components/public/NotificationManager";
 import InvalidParameter from "@/app/components/public/errors/InvalidParameter";
 import {Engine as QRCodeEngine} from "@/app/components/tools/qrcode/Engine";
@@ -17,47 +15,44 @@ export const Engine = () => {
         }
     };
 
-    const generateRoomId = () => {
-        const id = v4();
-        setRoomId(id);
-        return id;
-    };
-
-    const initPeer = () => {
-        const id = generateRoomId();
-        const peer = new Peer({initiator: true, trickle: false});
-
-        peer.on('signal', data => {
-            const roomUrl = `https://taco-preview.vercel.app/tools/drop/room/?roomId=${id}`;
-            const qrCodeComponent = generateQRCode(roomUrl);
-            setQrCode(qrCodeComponent);
-        });
-
-        peer.on('connect', () => {
-            if (selectedFile) {
-                const reader = new FileReader();
-                reader.onload = () => {
-                    if (reader.result) {
-                        peer.send(reader.result);
-                    }
-                };
-                reader.readAsArrayBuffer(selectedFile);
-            }
-        });
-    };
-
-    const generateFileQRCode = () => {
+    const uploadFile = async () => {
         if (!selectedFile) {
-            setError(new InvalidParameter("File"));
+            setError(new InvalidParameter("File non selezionato"));
             return;
         }
-        initPeer();
+
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+
+        try {
+            const response = await fetch('https://taco-api-nine.vercel.app/api/upload-file', {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Authorization': 'Bearer 55f02c20-d662-46ef-aa12-b98de0a04dff',
+                    'Access-Control-Request-Headers': 'Content-Type, Authorization',
+                },
+                body: formData
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setRoomId(data.roomID);
+                const roomUrl = `https://${window.location.host}/tools/drop/room/?roomId=${data.roomID}`;
+                const qrCodeComponent = generateQRCode(roomUrl);
+                setQrCode(qrCodeComponent);
+            } else {
+                setError(new Error('Error during file upload.'));
+            }
+        } catch (error) {
+            setError(new Error('Error during file upload.'));
+        }
     };
 
     return {
         selectedFile,
         handleFileChange,
-        generateFileQRCode,
+        uploadFile,
         qrCode,
         roomId
     };
--- app/tools/drop/room/page.tsx
+++ app/tools/drop/room/page.tsx
@@ -1,8 +1,120 @@
 'use client'
-import React from 'react';
 
-const Page = () => {
-    return (<></>);
+import React, {useEffect, useState, Suspense} from 'react';
+import {useSearchParams} from 'next/navigation';
+import TacoButton from "@/app/components/public/TacoButton";
+import Header from "@/app/components/public/Header";
+import Image from "next/image";
+import tacoDropLogo from "@/public/tacoDropLogo.png";
+import DarkModeEngine from "@/app/components/public/DarkModeEngine";
+import {NotificationManager} from "@/app/components/public/NotificationManager";
+import InvalidParameter from "@/app/components/public/errors/InvalidParameter";
+
+const RoomPage = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <RoomContent/>
+        </Suspense>
+    );
 };
 
-export default Page;
+const RoomContent = () => {
+    const searchParams = useSearchParams();
+    const roomId = searchParams.get('roomId');
+    const {darkMode} = DarkModeEngine();
+    const [fileURL, setFileURL] = useState<string | null>(null);
+    const [fileName, setFileName] = useState<string | null>(null);
+
+    const {setError} = NotificationManager();
+
+    const handleDownload = async () => {
+        const response = await fetch(fileURL!);
+        const blob = await response.blob();
+        const downloadUrl = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.download = fileName!;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(downloadUrl);
+        await deleteRoom();
+    };
+
+
+    const deleteRoom = async () => {
+        if (roomId) {
+            try {
+                const response = await fetch(`https://taco-api-nine.vercel.app/api/delete-room?roomID=${roomId}`, {
+                    method: 'GET',
+                    mode: "cors",
+                    headers: {
+                        'Authorization': 'Bearer 55f02c20-d662-46ef-aa12-b98de0a04dff',
+                        'Access-Control-Request-Headers': 'Content-Type, Authorization',
+                    },
+                });
+                if (!response.ok) {
+                    console.error('Error deleting room');
+                }
+            } catch (error) {
+                console.error('Error deleting room:', error);
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (!roomId) return;
+
+        const fetchFile = async () => {
+            try {
+                const response = await fetch(`https://taco-api-nine.vercel.app/api/get-file?roomID=${roomId}`, {
+                    method: 'GET',
+                    mode: "cors",
+                    headers: {
+                        'Authorization': 'Bearer 55f02c20-d662-46ef-aa12-b98de0a04dff',
+                        'Access-Control-Request-Headers': 'Content-Type, Authorization',
+                    },
+                });
+                if (response.status == 404) {
+                    return setError(new InvalidParameter("file"));
+                } else {
+                    const result = await response.json();
+                    setFileURL(result["fileData"]["url"]);
+                    setFileName(result["fileData"]["file_name"]);
+                }
+
+            } catch (error) {
+            }
+        };
+
+        fetchFile().then();
+
+    }, [roomId, setError]);
+
+    return (
+        <>
+            <div className={`${darkMode ? 'bg-taco-dark-primary' : 'bg-white'} text-gray-800 m-0 p-0`}>
+                <Header title={"taco | drop"} onSearchChange={null}/>
+                <main className="px-4 py-10 m-auto max-w-4xl sm:p-10">
+                    <div className="text-center p-5 m-auto">
+                        <div className="mx-auto max-w-md space-y-8">
+                            <div
+                                className={`${darkMode ? 'bg-taco-dark-secondary text-white' : 'bg-white text-black'} rounded-xl px-8 py-6 shadow-xl items-center`}>
+
+                                <Image src={tacoDropLogo}
+                                       className="font-bold text-2xl text-center m-auto"
+                                       alt="taco encoder"
+                                       width={100} height={100}/>
+                                <h1 className="font-bold text-2xl mb-6">{fileName || "Waiting for file..."}</h1>
+                                {fileURL ? (
+                                    <TacoButton type={"button"} text={"Download"} onClick={handleDownload}/>) : null}
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </>
+    );
+};
+
+export default RoomPage;
